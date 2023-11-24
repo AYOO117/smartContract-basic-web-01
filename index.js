82ec7f67af775879fc087f8d65d0aa24dda7ade0
Ujayata <62179207+Ujayata@users.noreply.github.com>
2023-11-24 17:09:38 +0100

    Update index.js
    **Potential Errors:**
    
    * **Line 27:** The `getBalance` function checks for `typeof window.ethereum != "undefined"` instead of `typeof window.ethereum !== "undefined"`. This could lead to an error if the `window.ethereum` object is undefined.
    
    **Possible Solution:**
    
    * Change `typeof window.ethereum != "undefined"` to `typeof window.ethereum !== "undefined"`.
    
    * **Line 36:** The `fund` function does not check if the `ethAmount` value is a valid number before parsing it with `ethers.utils.parseEther`. This could lead to an error if the `ethAmount` value is not a number.
    
    **Possible Solution:**
    
    * Add a check to ensure that `ethAmount` is a valid number before parsing it with `ethers.utils.parseEther`.
    
    **Potential Vulnerabilities:**
    
    * The code does not validate the input to the `fund` and `withdraw` functions. This could allow a malicious user to inject malicious code into the contract.
    
    **Possible Solution:**
    
    * Validate the input to the `fund` and `withdraw` functions to ensure that it is not malicious code.
    
    **Potential Bugs:**
    
    * The `listenForTransactionMine` function does not handle the case where the transaction fails to mine. This could cause the code to hang indefinitely.
    
    **Possible Solution:**
    
    * Add a timeout to the `listenForTransactionMine` function to prevent it from hanging indefinitely.

--- index.js
+++ index.js
@@ -1,82 +1,102 @@
-import { ethers } from "./ethers-5.2.esm.min.js"
-import { abi, contractAddress } from "./constants.js"
-const connectButton = document.getElementById("connectButton")
-const funButton = document.getElementById("fundButton")
-const balanceButton = document.getElementById("balanceButton")
-const withdrawButton = document.getElementById("withdrawButton")
+import { ethers } from "./ethers-5.2.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
 
-balanceButton.onclick = getBalance
-connectButton.onclick = connect
-fundButton.onclick = fund
-withdrawButton.onclick = withdraw
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+const balanceButton = document.getElementById("balanceButton");
+const withdrawButton = document.getElementById("withdrawButton");
 
-console.log(ethers)
+balanceButton.onclick = getBalance;
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+withdrawButton.onclick = withdraw;
+
+console.log(ethers);
 
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
-        console.log("there is a metamask")
-        await ethereum.request({
-            method: "eth_requestAccounts",
-        })
-        connectButton.innerHTML = "connected"
-    } else {
-        connectButton.innerHTML = "please install metamask"
-    }
-}
-async function getBalance() {
-    if (typeof window.ethereum != "undefined") {
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        const balance = await provider.getBalance(contractAddress)
-        console.log(ethers.utils.formatEther(balance))
-    }
-}
-async function fund() {
-    const ethAmount = document.getElementById("ethAmount").value
-    console.log(`funding with ${ethAmount}...`)
-    if (typeof window.ethereum != "undefined") {
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        const signer = provider.getSigner()
-        const contract = new ethers.Contract(contractAddress, abi, signer)
-
+        console.log("There is a MetaMask");
         try {
-            const transactionResponse = await contract.fund({
-                value: ethers.utils.parseEther(ethAmount),
-            })
-            await listenForTransactionMine(transactionResponse, provider)
-            console.log("Done!")
+            await ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            connectButton.innerHTML = "Connected";
         } catch (error) {
-            console.log("error")
+            console.error("Error connecting:", error.message);
+            connectButton.innerHTML = "Connection failed";
+        }
+    } else {
+        connectButton.innerHTML = "Please install MetaMask";
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const balance = await provider.getBalance(contractAddress);
+            console.log(ethers.utils.formatEther(balance));
+        } catch (error) {
+            console.error("Error getting balance:", error.message);
         }
     }
 }
-async function withdraw() {
-    if (typeof window.ethereum != "undefined") {
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        const signer = provider.getSigner()
-        const contract = new ethers.Contract(contractAddress, abi, signer)
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value;
+    console.log(`Funding with ${ethAmount}...`);
+    if (typeof window.ethereum !== "undefined") {
+        if (isNaN(ethAmount) || ethAmount <= 0) {
+            console.error("Invalid amount. Please enter a valid number.");
+            return;
+        }
 
         try {
-            const transactionResponse = await contract.withdraw()
-            console.log(provider, signer, contract)
-            await listenForTransactionMine(transactionResponse, provider)
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(contractAddress, abi, signer);
+
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            });
+
+            await listenForTransactionMine(transactionResponse, provider);
+            console.log("Done!");
         } catch (error) {
-            console.log(error)
+            console.error("Error funding:", error.message);
+        }
+    }
+}
+
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(contractAddress, abi, signer);
+
+            const transactionResponse = await contract.withdraw();
+            console.log(provider, signer, contract);
+            await listenForTransactionMine(transactionResponse, provider);
+        } catch (error) {
+            console.error("Error withdrawing:", error.message);
         }
     }
 }
 
 function listenForTransactionMine(transactionResponse, provider) {
-    console.log(`Mining ${transactionResponse.hash}`)
+    console.log(`Mining ${transactionResponse.hash}`);
     return new Promise((resolve, reject) => {
-        try {
-            provider.once(transactionResponse.hash, (transactionReceipt) => {
-                console.log(
-                    `Completed with ${transactionReceipt.confirmations} confirmations. `
-                )
-                resolve()
-            })
-        } catch (error) {
-            reject(error)
-        }
-    })
+        const timeout = setTimeout(() => {
+            reject(new Error("Transaction mining timed out"));
+        }, 120000); // Set timeout to 2 minutes
+
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            clearTimeout(timeout);
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations.`
+            );
+            resolve();
+        });
+    });
 }
